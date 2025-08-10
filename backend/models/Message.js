78ef590c623e60d04898f5b653188b2bb669ecee
FireNSpark <unfavorablejosh@gmail.com>
2025-08-10 15:49:48 -0400

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,29 +1,9 @@
-// models/Message.js
+
 import mongoose from 'mongoose';
 
-const messageSchema = new mongoose.Schema(
-  {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    read: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
-
-const Message = mongoose.model('Message', messageSchema);
-export default Message;
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  read: { type: Boolean, default: false }
+}, { timestamps: true });
