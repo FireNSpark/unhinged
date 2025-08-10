14e1bc85a2c10b43065ab9555ae73d65cd252024
FireNSpark <unfavorablejosh@gmail.com>
2025-08-10 08:00:31 -0400

    Update user.js

--- backend/user.js
+++ backend/user.js
@@ -1,1 +1,19 @@
 
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+    redFlags: [String],
+    greenFlags: [String],
+    aboutMe: String,
+    photos: [String],
+    quizResult: String,
+    badges: [String]
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', userSchema);
