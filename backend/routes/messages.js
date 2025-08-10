c5ef015cbec18489533185ed4f653043b2860eaa
FireNSpark <unfavorablejosh@gmail.com>
2025-08-10 12:55:55 -0400

    Update messages.js

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,13 +1,5 @@
-
 import express from 'express';
-import Message from '../models/Message.js';
-
 const router = express.Router();
 
-// GET /messages/:roomId
-router.get('/:roomId', async (req, res) => {
-  const list = await Message.find({ roomId: req.params.roomId }).sort({ createdAt: 1 });
-  res.json(list);
-});
-
+router.get('/', (_req, res) => res.json({ ok: true, route: 'messages' }));
 export default router;
